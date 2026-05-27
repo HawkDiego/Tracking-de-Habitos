3767c24eb92bc091a10b5fa0324ecdb526d7780c
Jhonatan <jhonatanperez1118591@correo.itm.edu.co>
2026-05-27 02:12:54 -0500

    feat: Diseño del Dashboard principal y maqueta de tabla de Auditorías

--- presentacion_web/Pages/Index.cshtml.cs
+++ presentacion_web/Pages/Index.cshtml.cs
@@ -1,13 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace presentacion_web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
-
+            // Aquí validaremos la Sesión más adelante para que no entren sin loguearse
+            // Y aquí mismo haremos el llamado asíncrono a la API de Auditorías
         }
     }
 }
