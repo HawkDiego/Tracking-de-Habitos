4417155cd1143a1ca1869132884e0b92aac457e4
Jhonatan <jhonatanperez1118591@correo.itm.edu.co>
2026-05-27 02:28:31 -0500

    feat: Diseño de la vista de Categorías con indicadores visuales

---
+++ presentacion_web/Pages/Categorias/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace presentacion_web.Pages.Categorias
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Próximamente: Llamado asíncrono a la API para listar categorías
+        }
+    }
+}
