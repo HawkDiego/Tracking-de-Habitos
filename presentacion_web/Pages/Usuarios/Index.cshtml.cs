e48e0b7e6ac40af9975359a2fad5fb54c3550fde
Jhonatan <jhonatanperez1118591@correo.itm.edu.co>
2026-05-27 02:23:28 -0500

    feat: Diseño de la vista de Gestión de Usuarios y Roles

---
+++ presentacion_web/Pages/Usuarios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace presentacion_web.Pages.Usuarios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Próximamente: Llamado asíncrono a la API para traer la lista de usuarios
+        }
+    }
+}
