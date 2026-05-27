c31f800328d67c93dc4bfd1e7e291c9d06f86ad5
Jhonatan <jhonatanperez1118591@correo.itm.edu.co>
2026-05-27 02:43:33 -0500

    feat: Diseño de la vista principal de Hábitos con barras de progreso

---
+++ presentacion_web/Pages/Habitos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace presentacion_web.Pages.Habitos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Próximamente: Llamado asíncrono a la API para listar los Hábitos
+        }
+    }
+}
